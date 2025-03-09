68256af56d7bbb97c12da34a5fa839a29f9e69fc
aldopqp <aldopqp@gmail.com>
2025-03-08 22:21:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RentalCalculator from "./components/RentalPriceCalculator";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RentalCalculator />
+    <App />
   </React.StrictMode>
 );
