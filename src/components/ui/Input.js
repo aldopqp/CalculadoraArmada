c24229b252712c28d1df7490ba94418129765ee7
aldopqp <aldopqp@gmail.com>
2025-03-09 09:35:14 -0300

    Create Input.js

---
+++ src/components/ui/Input.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Input({ type, value, onChange, className }) {
+  return (
+    <input type={type} value={value} onChange={onChange} className={`p-2 border rounded-md ${className}`} />;
+  );
+}
