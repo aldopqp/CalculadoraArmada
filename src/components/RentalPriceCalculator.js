a8b0fac883e710182c698eb7b1a879ef2e0b8966
aldopqp <aldopqp@gmail.com>
2025-03-09 09:34:05 -0300

    Update RentalPriceCalculator.js

--- src/components/RentalPriceCalculator.js
+++ src/components/RentalPriceCalculator.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../ui/Card"; 
+import { Button } from "../ui/Button"; 
+import { Input } from "../ui/Input"; 
 
 export default function RentalCalculator() {
   
